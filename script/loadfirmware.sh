f94d970e58729549225637af3192b89934d70ff5
Yi LIU <yiliu@zzu.edu.cn>
2023-10-11 23:03:29 +0800

    improve script

--- script/loadfirmware.sh
+++ script/loadfirmware.sh
@@ -4,29 +4,49 @@ DTBO_BASE_NAME="fpgafw"
 if [ $# -gt 0 ]
 then
     DTBO_BASE_NAME=$1
+    echo "Using default name of device-tree-overlay: <${DTBO_BASE_NAME}>."
 fi
 
-if [ ! -f /usr/lib/firmware/${DTBO_BASE_NAME}.dtbo ]
+echo "Search device-tree-overlay file: <${DTBO_BASE_NAME}>."
+DTBO_FILE_PATH=/usr/lib/firmware/${DTBO_BASE_NAME}.dtbo
+if [ ! -f ${DTBO_FILE_PATH} ]
 then
-    echo "unable to find /usr/lib/firmware/${DTBO_BASE_NAME}.dtbo"
+    echo "ERROR: Unable to find device-tree-overlay file at path: <${DTBO_FILE_PATH}>"
     exit -1
 fi
 
-if [ -d /configfs/device-tree/overlays/full ]
+CONFIGFS_DTBO_FULL_PATH=/configfs/device-tree/overlays/full
+if [ -d ${CONFIGFS_DTBO_FULL_PATH} ]
 then
-    rmdir /configfs/device-tree/overlays/full
+    echo "Overlay config path exists at configfs. Removing"
+    rmdir ${CONFIGFS_DTBO_FULL_PATH}
 fi
 
-echo 0 > /sys/class/fpga_manager/fpga0/flags
+echo "Configure fpga manager"
+SYSFS_FPGA_PATH=/sys/class/fpga_manager/fpga0
+if [ -d ${SYSFS_FPGA_PATH} ]
+then 
+    echo 0 > ${SYSFS_FPGA_PATH}/flags
+else
+    echo "ERROR: fpga manager is not avalible. Please enable it in the kernel."  
+    exit -1
+fi
 
+echo "Configure configfs"
 FINDMNT_RE=$(findmnt -T /configfs -t configfs)
 if [ $? != 0 ]
 then
-    echo "mounting configfs"
+    echo "Configfs does not exist. Mounting configfs"
     mount -t configfs configfs /configfs
+    if [ ! -d /configfs ]
+       echo "ERROR: Unable to mount configfs. Please enable it in the kernel."
+       exit -1
+    then
+else
+    echo "Configfs exists."
 fi
 
-mkdir -p /configfs/device-tree/overlays/full
+mkdir -p ${CONFIGFS_DTBO_FULL_PATH}
 
-echo "loading ${DTBO_BASE_NAME}"
-echo -n "${DTBO_BASE_NAME}.dtbo" > /configfs/device-tree/overlays/full/path
+echo "Loading device-tree-overlay: <${DTBO_BASE_NAME}>"
+echo -n "${DTBO_BASE_NAME}.dtbo" > ${CONFIGFS_DTBO_FULL_PATH}/path
--- script/loadmodule.sh
+++ script/loadmodule.sh
@@ -1,12 +1,89 @@
 #! /usr/bin/env bash
 
-BASH_SOURCE_REAL_PATH=$(readlink -m ${BASH_SOURCE})
-BASH_SOURCE_DIR=$(dirname ${BASH_SOURCE_REAL_PATH})
-BASH_SOURCE_BASE_NAME=$(basename $0)
+KO_BASE_NAME="fpgadma"
 
-KO_PATH=${BASH_SOURCE_DIR}/../module/fpgadma.ko
-KO_REAL_PATH=$(readlink -m ${KO_PATH})
+if [ $# -gt 0 ]
+then
+    KO_BASE_NAME=$1
+    echo "Name of kernel module : <${KO_BASE_NAME}>."
+else
+    echo "Using default name of kernel module : <${KO_BASE_NAME}>."
+fi
 
-echo "loading kernel module: ${KO_REAL_PATH}"
-insmod ${KO_REAL_PATH}
 
+SCRIPT_BASE_NAME=$(basename $0)
+
+CURRENT_DIR=$(pwd)
+
+
+SCRIPT_PATH=${BASH_SOURCE}
+SCRIPT_DIR=$(dirname ${SCRIPT_PATH})
+
+SCRIPT_REAL_PATH=$(readlink -m ${SCRIPT_PATH})
+SCRIPT_REAL_DIR=$(dirname ${SCRIPT_REAL_PATH})
+
+KO_PATH=
+
+if [ ! -n "${KO_PATH-}" ]
+then
+    KO_CONDIDATE_PATH=${CURRENT_DIR}/${KO_BASE_NAME}.ko
+    echo "Search for ${KO_CONDIDATE_PATH}"
+    if [ -f ${KO_CONDIDATE_PATH} ]
+    then
+	echo ".......... Found"
+	KO_PATH=${KO_CONDIDATE_PATH}
+    fi
+fi
+
+
+if [ ! -n "${KO_PATH-}" ]
+then
+    KO_CONDIDATE_PATH=${CURRENT_DIR}/${KO_BASE_NAME}.ko
+    echo "Search for ${KO_CONDIDATE_PATH}"
+    if [ -f ${KO_CONDIDATE_PATH} ]
+    then
+	echo ".......... Found"
+	KO_PATH=${KO_CONDIDATE_PATH}
+    fi
+fi
+
+if [ ! -n "${KO_PATH-}" ]
+then
+    KO_CONDIDATE_PATH=${SCRIPT_DIR}/${KO_BASE_NAME}.ko
+    echo "Search for ${KO_CONDIDATE_PATH}"
+    if [ -f ${KO_CONDIDATE_PATH} ]
+    then
+	echo ".......... Found"
+	KO_PATH=${KO_CONDIDATE_PATH}
+    fi
+fi
+
+if [ ! -n "${KO_PATH-}" ]
+then
+    KO_CONDIDATE_PATH=${SCRIPT_REAL_DIR}/${KO_BASE_NAME}.ko
+    echo "Search for ${KO_CONDIDATE_PATH}"
+    if [ -f ${KO_CONDIDATE_PATH} ]
+    then
+	echo ".......... Found"
+	KO_PATH=${KO_CONDIDATE_PATH}
+    fi
+fi
+
+if [ ! -n "${KO_PATH-}" ]
+then
+   KO_CONDIDATE_PATH=$(dirname ${SCRIPT_REAL_DIR})/module/${KO_BASE_NAME}.ko
+   echo "Search for ${KO_CONDIDATE_PATH}"
+   if [ -f ${KO_CONDIDATE_PATH} ] 
+   then
+       echo ".......... Found"
+       KO_PATH=${KO_CONDIDATE_PATH}
+   fi
+fi
+
+if [ ! -n "${KO_PATH-}" ]
+then
+    echo "ERROR: Unable to find ${KO_BASE_NAME}.ko"
+fi
+
+echo "Loading kernel module: ${KO_PATH}"
+insmod ${KO_PATH}
